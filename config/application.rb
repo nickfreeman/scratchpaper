c9ccd4d7a9ab0c997974120e89c0f842b04be7f5
Nick Freeman <nickafreeman@berkeley.edu>
2014-02-16 23:42:38 -0800

    fixing config

--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,10 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config.assets.initialize_on_precompile = false
-
 module Idealy
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
 
     # don't generate RSpec tests for views and helpers
     config.generators do |g|
