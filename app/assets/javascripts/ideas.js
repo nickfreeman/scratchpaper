669742747c6a0c1f527cf70c043a0413aaee5581
Nick Freeman <nickafreeman@berkeley.edu>
2014-04-12 04:05:24 -0700

    fixed js pipeline, added up voting

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,25 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require_tree .
-//= require bootstrap
-//= require masonry/jquery.masonry
-//= require masonry/jquery.event-drag
-//= require masonry/jquery.infinitescroll.min
-//= require masonry/modernizr-transitions
-
 $(function(){
 
 	adjustItems($(window).width());
@@ -41,6 +19,13 @@ $(function(){
             },
     isAnimated: true,
     isFitWidth: true
+  });
+
+  $(".like-button").click(function(){
+  	var likeCount = this.parentNode.getElementsByClassName('like-count')[0].innerHTML;
+  	likeCount++;
+  	this.parentNode.getElementsByClassName('like-count')[0].innerHTML = likeCount;
+  	console.log(likeCount);
   });
 
 });
