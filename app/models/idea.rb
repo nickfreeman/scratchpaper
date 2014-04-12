3c26585d8073c1f2249404dcdd73cf46cd5f4bd4
Nick Freeman <nickafreeman@berkeley.edu>
2014-04-12 02:30:55 -0700

    Added image upload, fixed background/dimensions

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -4,8 +4,7 @@ class Idea < ActiveRecord::Base
 
     belongs_to :user
 
-    has_attached_file :photo, :styles => { :large => "900x900>", :medium => "300x300>", :thumb => "100x100>" } #, :default_url => "/images/:style/missing.png"
-    validates_attachment :photo, #:presence => true,
-      :content_type => { :content_type => ["image/jpg", "image/gif", "image/png"] },
-      :size => { :in => 0..200.kilobytes }
+    has_attached_file :photo, :styles => { :large => "900x900>", :medium => "300x300>", :thumb => "100x100>" } , :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
+    validates_attachment :photo, :size => { :in => 0..200.kilobytes }
 end
