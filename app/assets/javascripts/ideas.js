acd8946caa6447ebbce4c615e15f517ba302c802
Nick Freeman <nickafreeman@berkeley.edu>
2014-04-12 19:16:36 -0700

    idea.js precompile

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -111,3 +111,16 @@ function getElementsByClassName(node,classname) {
     })(classname, node);
   }
 }
+
+function updateCountdown() {
+    // 140 is the max message length
+    var remaining = 140 - jQuery('#idea_blurb').val().length;
+    jQuery('#idea-countdown').text(remaining)
+    //.text(remaining + ' characters remaining');
+}
+
+jQuery(document).ready(function($) {
+    updateCountdown();
+    $('#idea_blurb').change(updateCountdown);
+    $('#idea_blurb').keyup(updateCountdown);
+});
