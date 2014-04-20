19637656c3a5a3eb30bd0edb9721513c20961f10
Nick Freeman <nickafreeman@berkeley.edu>
2014-04-19 18:23:43 -0700

    added avatars to users and avatar link

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ActiveRecord::Base
 
   has_many :ideas, dependent: :destroy
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "50x50>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
   validates_attachment :avatar, :size => { :in => 0..5.megabytes }
 
