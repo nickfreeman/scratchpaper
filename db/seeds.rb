ef246928230a58659b74b543d9c6fdc4a57bffc4
Nick Freeman <nickafreeman@berkeley.edu>
2014-04-24 11:51:17 -0700

    adding user to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -29,6 +29,9 @@ img9 = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'img9.jpg'))
 img10 = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'img10.jpg'))
 img11 = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'img11.jpg'))
 img12 = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'img12.jpg'))
+imgNick = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'nick.jpg'))
+imgAsaf = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'asaf.jpg'))
+imgJames = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'james.jpg'))
 
 
 ext_img1 = File.open(File.join(Rails.root, 'app', 'assets', 'images', 'tmp1.jpg'))
@@ -44,6 +47,9 @@ ext_imgArr = [ext_img1, ext_img2, ext_img3, ext_img4, ext_img5, ext_img6]
 
 test_user_1 = User.create(:name => 'user 1', :email => 'test@test.com', :password => 'qwerasdf', :avatar => img1)
 test_user_2 = User.create(:name => 'user 2', :email => 'test2@test.com', :password => 'qwerasdf', :avatar => img3)
+nick_user = User.create(:name => 'Nick', :email => 'nick@nick.com', :password => 'qwerasdf', :avatar => imgNick)
+asaf_user = User.create(:name => 'Asaf', :email => 'asaf@asaf.com', :password => 'qwerasdf', :avatar => imgAsaf)
+james_user = User.create(:name => 'James', :email => 'james@james.com', :password => 'qwerasdf', :avatar => imgJames)
 
 
 99.times do |i|
@@ -58,13 +64,13 @@ test_user_2 = User.create(:name => 'user 2', :email => 'test2@test.com', :passwo
 	end
 end
 
-Idea.create(:blurb => "Import a video, and see how the camera moved as the shot filmed, using a virtual person holding a camera.", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'photography.jpg')), :problem => "We are currently unable to learn filming techniques without in-person training.  Many of my friend spend hundreds of dollars on lessons in cinamatogrphy, learning the best angles for the shots that we want to include in our films. Watching videos definitely helps to learn cinematography, but seeing what is done behind the camera for every shot would be very useful.", :solution => "I imagine a solution where a device can be added to a camera during a shot, and the motion is detected to recoginze the speed and direction of the camera's motion, as well as the ways it is tilted and turned. If this motion can then be translated into a virtual camera man which is doing the filming, it could help teach aspiring cinematographers the ways that very intricate shots are done.")
-Idea.create(:blurb => "GPS with routes that maximize MPG", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'audi.jpg')))
-Idea.create(:blurb => "A Browser Extension That Lets You Choose What You Censor")
-Idea.create(:blurb => "Fundraising website that lets common people tell politicians if you vote for X we will donate $ to whoever runs against you")
-Idea.create(:blurb => "Jukebox app for bars", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'jukebox.jpg')))
-Idea.create(:blurb => "A resume that illustrates your present ability rather than your employment history")
-Idea.create(:blurb => "A microwave with a built-in whisk/stirrer so you don't have to remove your food every two minutes and stir it")
-Idea.create(:blurb => "Wind turbines along highways and interstates", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'turbine.jpg')))
-Idea.create(:blurb => "A website that tells you if you will need the assigned textbook for that class/professor")
-Idea.create(:blurb => "Import a video, and see how the camera moved as the shot filmed, using a virtual person holding a camera.", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'photography.jpg')), :problem => "We are currently unable to learn filming techniques without in-person training.  Many of my friend spend hundreds of dollars on lessons in cinamatogrphy, learning the best angles for the shots that we want to include in our films. Watching videos definitely helps to learn cinematography, but seeing what is done behind the camera for every shot would be very useful.", :solution => "I imagine a solution where a device can be added to a camera during a shot, and the motion is detected to recoginze the speed and direction of the camera's motion, as well as the ways it is tilted and turned. If this motion can then be translated into a virtual camera man which is doing the filming, it could help teach aspiring cinematographers the ways that very intricate shots are done.")
+Idea.create(:blurb => "Import a video, and see how the camera moved as the shot filmed, using a virtual person holding a camera.", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'photography.jpg')), :problem => "We are currently unable to learn filming techniques without in-person training.  Many of my friend spend hundreds of dollars on lessons in cinamatogrphy, learning the best angles for the shots that we want to include in our films. Watching videos definitely helps to learn cinematography, but seeing what is done behind the camera for every shot would be very useful.", :solution => "I imagine a solution where a device can be added to a camera during a shot, and the motion is detected to recoginze the speed and direction of the camera's motion, as well as the ways it is tilted and turned. If this motion can then be translated into a virtual camera man which is doing the filming, it could help teach aspiring cinematographers the ways that very intricate shots are done.", :user => nick_user)
+Idea.create(:blurb => "GPS with routes that maximize MPG", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'audi.jpg')), :user => james_user)
+Idea.create(:blurb => "A Browser Extension That Lets You Choose What You Censor", :user => asaf_user)
+Idea.create(:blurb => "Fundraising website that lets common people tell politicians if you vote for X we will donate $ to whoever runs against you", :user => nick_user)
+Idea.create(:blurb => "Jukebox app for bars", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'jukebox.jpg')), :user => james_user)
+Idea.create(:blurb => "A resume that illustrates your present ability rather than your employment history", :user => asaf_user)
+Idea.create(:blurb => "A microwave with a built-in whisk/stirrer so you don't have to remove your food every two minutes and stir it", :user => nick_user)
+Idea.create(:blurb => "Wind turbines along highways and interstates", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'turbine.jpg')), :user => james_user)
+Idea.create(:blurb => "A website that tells you if you will need the assigned textbook for that class/professor", :user => asaf_user)
+Idea.create(:blurb => "Import a video, and see how the camera moved as the shot filmed, using a virtual person holding a camera.", :photo => File.open(File.join(Rails.root, 'app', 'assets', 'images', 'photography.jpg')), :problem => "We are currently unable to learn filming techniques without in-person training.  Many of my friend spend hundreds of dollars on lessons in cinamatogrphy, learning the best angles for the shots that we want to include in our films. Watching videos definitely helps to learn cinematography, but seeing what is done behind the camera for every shot would be very useful.", :solution => "I imagine a solution where a device can be added to a camera during a shot, and the motion is detected to recoginze the speed and direction of the camera's motion, as well as the ways it is tilted and turned. If this motion can then be translated into a virtual camera man which is doing the filming, it could help teach aspiring cinematographers the ways that very intricate shots are done.", :user => nick_user)
