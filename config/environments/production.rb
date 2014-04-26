d37192cdb617b54365868af265c9bdab62b9e17a
Nick Freeman <nickafreeman@berkeley.edu>
2014-04-25 21:31:48 -0700

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Idealy::Application.configure do
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
   # config.assets.precompile += %w( search.js )
-  config.assets.precompile += %w( idea.js lightbox.min.js show.js imagelightbox.js imagelightbox.min.js )
+  config.assets.precompile += %w( ideas.js lightbox.min.js show.js imagelightbox.js imagelightbox.min.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
