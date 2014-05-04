2da272e90c19ea2bdb1ee714a77bd472e24d478f
Nick Freeman <nickafreeman@berkeley.edu>
2014-05-04 16:45:21 -0700

    profile image upload almost complete

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -8,4 +8,13 @@ class ProfileController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
+
+  def update
+  	@user = current_user
+    if @user.update_attributes(params[:avatar])
+      redirect_to profile_index_path, :notice => "Profile updated."
+    else
+      redirect_to profile_index_path, :alert => "Unable to update profile."
+    end
+  end
 end
